db4927fa43b4b01e094a77a08565f910eb5e43cd
ROYIANS <zhangyj.ie@gennergy.com>
2026-04-02 11:39:56 +0800

    fix: use reference project traffic light SVG icons (close/minimize/stretch)

--- packages/shell/src/components/WindowFrame/TrafficLights.tsx
+++ packages/shell/src/components/WindowFrame/TrafficLights.tsx
@@ -1,10 +1,48 @@
-import { Icon } from '@iconify/react'
 import { useWindowStore } from '../../stores/useWindowStore'
 import styles from './TrafficLights.module.css'
 
 interface TrafficLightsProps {
   windowId: string
   focused: boolean
+}
+
+function CloseIcon() {
+  return (
+    <svg width={7} height={7} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        stroke="#000"
+        strokeWidth={1.2}
+        strokeLinecap="round"
+        d="M1.182 5.99L5.99 1.182m0 4.95L1.182 1.323"
+      />
+    </svg>
+  )
+}
+
+function MinimizeIcon() {
+  return (
+    <svg width={6} height={1} fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path stroke="#000" strokeWidth={2} strokeLinecap="round" d="M.61.703h5.8" />
+    </svg>
+  )
+}
+
+function MaximizeIcon() {
+  return (
+    <svg
+      viewBox="0 0 13 13"
+      width={10}
+      height={10}
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit={2}
+    >
+      <path d="M4.871 3.553L9.37 8.098V3.553H4.871zm3.134 5.769L3.506 4.777v4.545h4.499z" />
+      <circle cx={6.438} cy={6.438} r={6.438} fill="none" />
+    </svg>
+  )
 }
 
 export function TrafficLights({ windowId, focused }: TrafficLightsProps) {
@@ -27,7 +65,7 @@ export function TrafficLights({ windowId, focused }: TrafficLightsProps) {
         onClick={() => closeWindow(windowId)}
         onMouseDown={stopPropagation}
       >
-        <Icon icon="ri:close-line" width={8} height={8} color="#4d0000" />
+        <CloseIcon />
       </button>
       <button
         className={styles.minimizeLight}
@@ -35,7 +73,7 @@ export function TrafficLights({ windowId, focused }: TrafficLightsProps) {
         onClick={() => setWindowState(windowId, 'minimized')}
         onMouseDown={stopPropagation}
       >
-        <Icon icon="ri:subtract-line" width={8} height={8} color="#4d3400" />
+        <MinimizeIcon />
       </button>
       <button
         className={styles.maximizeLight}
@@ -43,7 +81,7 @@ export function TrafficLights({ windowId, focused }: TrafficLightsProps) {
         onClick={() => toggleMaximize(windowId)}
         onMouseDown={stopPropagation}
       >
-        <Icon icon="ri:fullscreen-line" width={8} height={8} color="#004d00" />
+        <MaximizeIcon />
       </button>
     </div>
   )
